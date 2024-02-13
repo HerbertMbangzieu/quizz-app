cc4ebf6cda380fb0f754419d60fad061861b43b1
Herbert Mbangzieu <herbertmbangzieu@gmail.com>
2024-02-13 08:29:56 -0500

    reset-index-and-routes

--- backend/routes/userRouters.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import express, { Router, request, response } from "express";
-import {User} from '../models/userModel.js'
-
-const router = Router();
-router.get("/users", (request, response) => {
-  const users = User.find({})
-});
